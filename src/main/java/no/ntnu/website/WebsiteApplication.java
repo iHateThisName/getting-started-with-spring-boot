138864f9b871823d3073781caada91f8b7873ee4
Ivar Bredeli <ivabr@stud.ntnu.no>
2022-03-26 20:30:13 +0100

    31% Done, Student Class

--- src/main/java/no/ntnu/website/WebsiteApplication.java
+++ src/main/java/no/ntnu/website/WebsiteApplication.java
@@ -1,9 +1,14 @@
 package no.ntnu.website;
 
+import no.ntnu.website.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -14,8 +19,14 @@ public class WebsiteApplication {
 	}
 
 	@GetMapping
-	public String hello() {
-		return "Hello World";
+	public List<Student> hello() {
+		return List.of(new Student(
+				1L,
+				"Mariam",
+				"mariam.jamal@gmail.com",
+				LocalDate.of(2000, Month.JANUARY, 5),
+				21
+		));
 	}
 
 }
