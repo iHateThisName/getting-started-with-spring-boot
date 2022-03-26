4b2a8305ae2b6a657ec0a810ad94587696c48c2d
Ivar Bredeli <ivabr@stud.ntnu.no>
2022-03-26 20:19:05 +0100

    28% Done, Hello World

--- src/main/java/no/ntnu/website/WebsiteApplication.java
+++ src/main/java/no/ntnu/website/WebsiteApplication.java
@@ -2,12 +2,20 @@ package no.ntnu.website;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class WebsiteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebsiteApplication.class, args);
+	}
+
+	@GetMapping
+	public String hello() {
+		return "Hello World";
 	}
 
 }
